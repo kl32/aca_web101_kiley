be617762afeff59335cf867da8963f00f9857fdf
Kiley Lewis <kileylewis32@gmail.com>
2019-03-18 17:04:33 -0500

    update

--- flowerClick/flower.js
+++ flowerClick/flower.js
@@ -1,28 +1,9 @@
+var flowers = 0;
 
-    
-    var flowers = 0;
+function flowerClick(number) {
+    flowers = flowers + number;
+    document.getElementById("flowers").innerHTML = flowers;
+};
 
-    function flowerClick(number){
-        flowers = flowers + number;
-        document.getElementById("flowers").innerHTML =  cookies;
-    };
 
-    var cursors = 0;
 
-    function buyCursor(){
-        var cursorCost = Math.floor(10 * Math.pow(1.1,cursors));
-        if(flowers >= cursorCost){
-            cursors = cursors + 1;
-            flowers = floweers - cursorCost;
-            document.getElementById('cursors').innerHTML = cursors;
-            document.getElementById('flowers').innerHTML = flowers;
-        };
-
-        var nextCost = Math.floor(10 * Math.pow(1.1,cursors));
-        document.getElementById('cursorCost').innerHTML = nextCost;
-    };
-
-    window.setInterval(function(){
-
-        flowerClick(cursors);
-    }, 1000);
